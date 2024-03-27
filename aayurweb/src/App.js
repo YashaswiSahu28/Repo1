9314f0f5fd9b32fcd5aa14af1709d81af58b45ab
Yashaswi Sahu <yashaswikrsahu@gmail.com>
2024-03-27 17:39:47 +0530

    init

---
+++ aayurweb/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aayurscan</h1>
+      <Layout/>
+    </div>
+  );
+}
+
+export default App;
